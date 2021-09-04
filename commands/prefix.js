67f40f6cebfb87a7a011833a343b7edae2487509
Gooselad <63563412+scalgoon@users.noreply.github.com>
2021-09-04 16:34:08 -0500

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,8 +2,6 @@ const db = require('quick.db');
 
 module.exports.run = async (client, message, args) => {
 
-    try {
-
         if (!message.member.permissions.has('ADMINISTRATOR')) return;
 
         if (!args[0]) return message.channel.send("Please provide a new prefix for me to set")
@@ -17,23 +15,6 @@ module.exports.run = async (client, message, args) => {
 
 
         message.channel.send(`My prefix is now **${args[0]}**`);
-
-    } catch (e) {
-
-        let reembed = new MessageEmbed()
-            .setColor("RED")
-            .setTitle("Command Error")
-            .addField("Command", "prefix")
-            .addField("Guild", `${message.guild.name} (${message.guildId})`)
-            .addField("Error:", `> ${e}`)
-            .setTimestamp()
-
-        await client.users.cache.get("734784924619505774").send({ embeds: [reembed] });
-
-        message.reply("An error has occured and has been reported to my creator!");
-    }
-
-
 }
 
 module.exports.config = {
