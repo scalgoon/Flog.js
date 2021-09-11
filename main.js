d954952c2cd7c7a7f28d11d8d1a7e572c4fb0358
Gooselad <63563412+scalgoon@users.noreply.github.com>
2021-09-11 15:16:30 -0500

    Added anti-spam

--- main.js
+++ main.js
@@ -329,6 +329,130 @@ client.on('interactionCreate', (interaction) => {
     }
 })
 
+const usersMap = new Map();
+const LIMIT = 5;
+const BEFLIMIT = 3;
+const TIME = 7000;
+const DIFF = 3000;
+
+client.on("messageCreate", async (message) => {
+
+    if(message.author.bot) return;
+
+    let ch = db.fetch(`antispam_${message.guild.id}`);
+
+    if (ch === null) {
+        return;
+    }else{
+
+     if (!message.member.permissions.has('ADMINISTRATOR')) {
+
+    if (usersMap.has(message.author.id)) {
+        const userData = usersMap.get(message.author.id);
+        const { lastMessage, timer } = userData;
+        const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+        let msgCount = userData.msgCount;
+
+        if (difference > DIFF) {
+            clearTimeout(timer);
+            userData.msgCount = 1;
+            userData.lastMessage = message;
+            userData.timer = setTimeout(() => {
+                usersMap.delete(message.author.id);
+            }, TIME);
+            usersMap.set(message.author.id, userData)
+        }
+        else {
+            ++msgCount;
+            if (parseInt(msgCount) === BEFLIMIT) {
+
+                let mut = new MessageEmbed()
+                .setAuthor(`${message.guild.name}`, message.guild.iconURL())
+                .setDescription(`If you continue to spam you will be muted!`)
+                .setColor("RED")
+
+                message.author.send({ embeds: [mut] });
+            }
+            if (parseInt(msgCount) === LIMIT) {
+                let muterole = db.get(`mrole_${message.guild.id}`);
+
+                if (muterole === null) {
+                    return message.channel.send("Please set a muted role to complete this action")
+                };
+
+                message.member.roles.add(muterole);
+
+                let amount = 1
+
+                db.add(`cases_${message.guild.id}`, amount);
+
+                let cases = db.fetch(`cases_${message.guild.id}`)
+
+                let data = {
+                    staff: `\n**Case ${cases}**\nModerator: <@${client.user.id}>\nType: Mute\nDurration: 15m\nReason: Spam`,
+                }
+
+                db.push(`logsdb_${message.guild.id}_${message.author.id}`, data)
+
+                let embed = new MessageEmbed()
+                    .setTitle(`Case Number ${cases}`)
+                    .setDescription(`<@${message.author.id}> has been muted for 15m`)
+                    .setFooter("Auto-Mute")
+                    .setColor("GREEN")
+
+                message.channel.send({ embeds: [embed] })
+
+                let chx = db.get(`botlgs_${message.guild.id}`);
+
+                if (chx === null) {
+                    return message.channel.send("Please set a bot logs channel if you want command logs")
+                }
+
+                let muted = new MessageEmbed()
+                    .setAuthor(`${message.guild.name}`, message.guild.iconURL())
+                    .setDescription(`<@${message.author.id}> has been auto-muted`)
+                    .addField("Moderator", `Name: <@${client.user.id}> \nID: ${client.user.id}`)
+                    .addField("User", `Name: ${message.author.tag} \nID: ${message.author.id}`)
+                    .addField(`Reason`, `Spam`)
+                    .addField("Duration", `15m`)
+                    .setColor("BLUE")
+                    .setTimestamp()
+
+                client.channels.cache.get(chx).send({ embeds: [muted] });
+
+                setTimeout(() => {
+                    message.member.roles.remove(muterole);
+
+                    let unm = new MessageEmbed()
+                        .setAuthor(`${message.guild.name}`, message.guild.iconURL())
+                        .setDescription(`<@${message.author.id}> has been unmuted`)
+                        .addField("Moderator", `Name: <@826570548783087627> \nID: 826570548783087627`)
+                        .addField("User", `Name: ${message.author.tag} \nID: ${message.author.id}`)
+                        .setColor("BLUE")
+                        .setTimestamp()
+
+                    client.channels.cache.get(chx).send({ embeds: [unm] });
+                }, 900000);
+            } else {
+                userData.msgCount = msgCount;
+                usersMap.set(message.author.id, userData);
+            }
+        }
+    }
+    else {
+        let fn = setTimeout(() => {
+            usersMap.delete(message.author.id);
+        }, TIME);
+        usersMap.set(message.author.id, {
+            msgCount: 1,
+            lastMessage: message,
+            timer: fn
+        });
+    }
+}
+}
+})
+
 
 client.login(config.token);
 
