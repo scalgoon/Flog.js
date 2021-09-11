21895a0259577d403abe560dc43e1a033bcd3de9
Gooselad <63563412+scalgoon@users.noreply.github.com>
2021-09-11 15:23:23 -0500

    removed mongo code

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,22 +1,5 @@
-const mongoose = require('mongoose');
-
-const Guild = require('../models/guild');
-
-const message = require('./messageCreate');
-
-const config = require('../config.json');
-
 module.exports = async (client, guild) => {
-    guild = new Guild({
-        _id: mongoose.Types.ObjectId(),
-        guildID: guild.id,
-        guildName: guild.name,
-        prefix: config.prefix
-    });
 
-    guild.save()
-    .then(result => console.log(result))
-    .catch(err => console.error(err));
 
     if (guild.systemChannel) {
         guild.systemChannel.send("Hello I'm Terminal!\nTo see my commands do *help").catch(e => console.log("failed to send message due to error: ", e))
